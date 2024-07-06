a890fc9e632cecaae46a3c4ffb5f520c760a711b
prasanjitjena <prasanjit@factonity.com>
2024-07-06 17:38:48 +0530

    updated views

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def home_page_view(request,*args,**kwargs):
     template = "home.html"
     context = {
-        "title":'Home Page'
+        "title":'Home Page',
+        'user':request.user
     }
     return render(request,template_name=template,context=context)
